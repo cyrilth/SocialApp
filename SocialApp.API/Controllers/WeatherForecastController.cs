e870dab00bfc69bccffdb7b1d8e65d583130dfba
cyrilth <cyrilth@users.noreply.github.com>
2019-10-23 15:01:18 -0400

    Frontend Register and Auth Setup (#1)

--- SocialApp.API/Controllers/WeatherForecastController.cs
+++ SocialApp.API/Controllers/WeatherForecastController.cs
@@ -24,6 +24,7 @@ namespace SocialApp.API.Controllers
             _logger = logger;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
