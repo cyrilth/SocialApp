aad50b97d8d27a4c75f6cf7d6b222be278093b05
Cyril Thomas <cyrilthomas@hotmail.com>
2019-10-24 00:09:50 -0400

    Added Gobal Error Handler

--- SocialApp.API/Controllers/AuthController.cs
+++ SocialApp.API/Controllers/AuthController.cs
@@ -45,6 +45,8 @@ namespace SocialApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+            throw new Exception("Computer says no!");
+
             var userFormRepo = await _repo.Login(userForLoginDto.Username, userForLoginDto.Password);
 
             if (userFormRepo == null)
--- SocialApp.API/Startup.cs
+++ SocialApp.API/Startup.cs
@@ -15,6 +15,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using SocialApp.API.Helpers;
 
 namespace SocialApp.API
 {
@@ -52,6 +56,21 @@ namespace SocialApp.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(builder => {
+                    builder.Run(async context => {
+                        context.Response.StatusCode= (int)HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if(error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
             }
             
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
