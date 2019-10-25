d7d9d8db11f71df36ef675d629b67b038057a0aa
cyrilth <cyrilth@users.noreply.github.com>
2019-10-25 16:40:16 -0400

    Develop (#4)
    * Frontend Register and Auth Setup
    
    * Added Gobal Error Handler
    
    * Removed Unwanted proj file
    
    * Added Angular HttpInterceptor for Error Handling
    
    * Added Alertifyjs, Auth0 Angular JWT, NGX BootStrap & Bootswatch minty Theme
    
    * Added Packages info to Readme
    
    * Added Routing to Angular
    
    * Added Automapper and Extended
    the User Class to include Photo.
    
    * Fixed duplicate code

---
+++ SocialApp.API/Data/ISocialRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SocialApp.API.Models;
+
+namespace SocialApp.API.Data
+{
+    public interface ISocialRepository
+    {
+         void Add<T>(T entity) where T: class;
+         void Delete<T>(T entity) where T: class;
+         Task<bool> SaveAll();
+         Task<IEnumerable<User>> GetUsers();
+         Task<User> GetUser(int id);
+    }
+}
--- SocialApp.API/Dtos/UserForDetailedDto.cs
+++ SocialApp.API/Dtos/UserForDetailedDto.cs
@@ -1,38 +1,37 @@
 using System;
 using System.Collections.Generic;
+using SocialApp.API.Models;
 
-namespace SocialApp.API.Models
+namespace SocialApp.API.Dtos
 {
-    public class User
+    public class UserForDetailedDto
     {
         public int Id { get; set; }
 
         public string Username { get; set; }
-
-        public byte[] PasswordHash { get; set; }
-
-        public byte[] PasswordSalt { get; set; }
-
+        
         public string Gender { get; set; }
-
-        public DateTime DateOfBirth { get; set; }
-
+        
+        public int Age { get; set; }
+        
         public string KnownAs { get; set; }
+        
         public DateTime Created { get; set; }
-
+        
         public DateTime LastActive { get; set; }
-
+        
         public string Introduction { get; set; }
         
         public string LookingFor { get; set; }
-
-        public string Interrests { get; set; }
-
+        
+        public string Interests { get; set; }
+        
         public string City { get; set; }
-
+        
         public string Country { get; set; }
-
-        public ICollection<Photo> Photos { get; set; }
-
+        
+        public string PhotoUrl { get; set; }
+        
+        public ICollection<PhotosForDetailedDto> Photos { get; set; }
     }
 }
