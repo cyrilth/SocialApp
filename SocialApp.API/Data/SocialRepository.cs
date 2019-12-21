0cb7d6a32576cd578d882605c39bb29022332889
Cyril Thomas <cyrilthomas@hotmail.com>
2019-12-21 13:37:39 -0500

    Added API Support For Pagination

--- SocialApp.API/Data/SocialRepository.cs
+++ SocialApp.API/Data/SocialRepository.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SocialApp.API.Helpers;
 using SocialApp.API.Models;
 
 namespace SocialApp.API.Data
@@ -43,11 +44,11 @@ namespace SocialApp.API.Data
             return user;
         }
 
-        public async Task<IEnumerable<User>> GetUsers()
+        public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users = await _context.Users.Include(p => p.Photos).ToListAsync();
+            var users = _context.Users.Include(p => p.Photos);
 
-            return users;
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<bool> SaveAll()
